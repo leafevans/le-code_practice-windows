ace0476e349df05bfdf19f89e66404f0a49eb115
LeafEvans <3186646217@qq.com>
2023-12-12 10:56:21 +0800

    2023_12_12

---
+++ Homework/test_2023_12_12_8_46.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int changeNum(int num)
+{
+    int new_num = 0;
+
+    while (num)
+    {
+        if(num % 10 % 2 == 0)
+        {
+            new_num = new_num * 10;
+        }
+        else
+        {
+            new_num = new_num * 10 + 1;
+        }
+        num /= 10;
+    }
+
+    return new_num;
+}
+
+int main(void)
+{
+    int n = 0;
+    scanf("%d", &n);
+    printf("%d\n", changeNum(n));
+    return 0;
+}
---
+++ Homework/test_2023_12_12_9_30.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void)
+{
+    float price = 0.0f;
+    int m = 0;
+    int d = 0;
+    int flag = 0;
+
+    scanf("%f %d %d %d", &price, &m, &d, &flag);
+    if (m == 12 && d == 12)
+    {
+        price = price * 0.8 - flag * 50.0;
+    }
+    else if (m == 11 && d == 11)
+    {
+        price = price * 0.7 - flag * 50.0;
+    }
+
+    if (price <= 0.0f)
+    {
+        printf("%.2f\n", 0.00);
+    }
+    else
+    {
+        printf("%.2f\n", price);
+    }
+
+    return 0;
+}
