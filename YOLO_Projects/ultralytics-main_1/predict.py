27ae910fc6c51d9717b3e84af7a69b722408c162
LeafEvans <140964616+LeafEvans@users.noreply.github.com>
2025-03-16 01:17:36 +0800

    Update files

--- YOLO_Projects/ultralytics-main_1/predict.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-# 加载模型并设置任务类型为检测
-yolo = YOLO("./yolov8n.pt", task='detect')
-
-# 执行预测
-result = yolo(source='./ultralytics/assets/tunnel.jpg', save=True, conf=0.05)
--- YOLO_Projects/ultralytics-main_1/train.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-# Load a model
-model = YOLO('yolov8n.pt')
-
-# Train the model
-model.train(data='yolov8-fog.yaml', workers=0, epochs=30, batch=16)
