6208f7ce8e96dcab99f8a69ce61f8b6dcce08047
LeafEvans <140964616+LeafEvans@users.noreply.github.com>
2025-09-29 15:22:59 +0800

    Update files

--- practice/go/test_2025_09_28/main.go
+++ practice/go/test_2025_09_28/main.go
@@ -576,4 +576,401 @@
 //		<-ch3
 //		ch3 <- 44
 //	}
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	ch := make(chan int, 10)
+
+// 	for i := 1; i <= 10; i++ {
+// 		ch <- i
+// 	}
+// 	close(ch)
+
+//		for val := range ch {
+//			fmt.Print(val, " ")
+//		}
+//		fmt.Println()
+//	}
+// package main
+
+// import (
+// 	"fmt"
+// 	"sync"
+// 	"time"
+// )
+
+// // 写数据
+// func write(ch chan int, wg *sync.WaitGroup) {
+// 	defer wg.Done()
+// 	for i := 1; i <= 10; i++ {
+// 		ch <- i
+// 		fmt.Printf("写入数据：%v\n", i)
+// 		time.Sleep(500 * time.Millisecond)
+// 	}
+// 	close(ch)
+// }
+
+// // 读数据
+// func read(ch chan int, wg *sync.WaitGroup) {
+// 	defer wg.Done()
+// 	for v := range ch {
+// 		fmt.Printf("读取数据：%v\n", v)
+// 		time.Sleep(50 * time.Millisecond)
+// 	}
+// }
+
+//	func main() {
+//		var wg sync.WaitGroup
+//		ch := make(chan int, 10)
+//		wg.Add(2)
+//		go write(ch, &wg)
+//		go read(ch, &wg)
+//		wg.Wait()
+//		fmt.Println("执行完毕")
+//	}
+// package main
+
+// import "fmt"
+
+//	func main() {
+//		ch := make(chan int, 1)
+//		ch <- 10
+//		fmt.Println("发送成功")
+//	}
+// package main
+
+// import "fmt"
+
+//	func main() {
+//		ch := make(chan int, 1)
+//		ch <- 10
+//		ch <- 12
+//		fmt.Println("发送成功")
+//	}
+// package main
+
+// import "fmt"
+
+//	func main() {
+//		ch := make(chan int, 1)
+//		ch <- 10 // 放入
+//		<-ch     // 取走
+//		ch <- 12 // 放入
+//		<-ch     // 取走
+//		ch <- 17 // 放入
+//		fmt.Println("发送成功")
+//	}
+// package main
+
+// import (
+// 	"fmt"
+// 	"sync"
+// 	"time"
+// )
+
+// func countPrimes(start, end int, wg *sync.WaitGroup, result *int) {
+// 	defer wg.Done()
+// 	count := 0
+// 	for num := start; num < end; num++ {
+// 		isPrime := true
+// 		for i := 2; i*i < num; i++ {
+// 			if num%i == 0 {
+// 				isPrime = false
+// 				break
+// 			}
+// 		}
+// 		if isPrime {
+// 			count++
+// 		}
+// 	}
+// 	*result = count
+// }
+
+// func main() {
+// 	start := time.Now()
+// 	var wg sync.WaitGroup
+// 	parts := 6
+// 	interval := 1.2e6 / parts
+// 	results := make([]int, parts)
+
+// 	for i := range parts {
+// 		wg.Add(1)
+// 		s := i * interval
+// 		e := (i + 1) * interval
+// 		go countPrimes(s, e, &wg, &results[i])
+// 	}
+// 	wg.Wait()
+
+//		total := 0
+//		for _, v := range results {
+//			total += v
+//		}
+//		fmt.Println(total)
+//		fmt.Println(time.Since(start))
+//	}
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	ch := make(chan int, 5)
+
+// 	for i := range 5 {
+// 		ch <- i + 1
+// 	}
+// 	close(ch)
+
+//		for v := range ch {
+//			fmt.Print(v, " ")
+//		}
+//		fmt.Println()
+//	}
+// package main
+
+// import (
+// 	"fmt"
+// 	"sync"
+// 	"time"
+// )
+
+// func write(ch chan int, wg *sync.WaitGroup) {
+// 	defer wg.Done()
+// 	for i := range 10 {
+// 		ch <- i + 1
+// 		fmt.Printf("写入数据：%v\n", i+1)
+// 		time.Sleep(time.Millisecond * 100)
+// 	}
+// 	close(ch)
+// }
+
+// func read(ch chan int, wg *sync.WaitGroup) {
+// 	defer wg.Done()
+// 	for v := range ch {
+// 		fmt.Printf("读取数据：%v\n", v)
+// 		time.Sleep(time.Millisecond * 50)
+// 	}
+// }
+
+//	func main() {
+//		var wg sync.WaitGroup
+//		ch := make(chan int)
+//		wg.Add(2)
+//		go write(ch, &wg)
+//		go read(ch, &wg)
+//		wg.Wait()
+//		fmt.Println("读取完毕……")
+//	}
+// package main
+
+// import (
+// 	"fmt"
+// 	"sync"
+// 	"time"
+// )
+
+// func generateNumbers(numChan chan int, wg *sync.WaitGroup) {
+// 	defer wg.Done()
+// 	for num := 2; num < 120; num++ {
+// 		numChan <- num
+// 	}
+// 	close(numChan)
+// }
+
+// func filterPrimes(numChan chan int, primeChan chan int, exitChan chan bool, wg *sync.WaitGroup) {
+// 	defer wg.Done()
+// 	for num := range numChan {
+// 		isPrime := true
+// 		for i := 2; i*i <= num; i++ {
+// 			if num%i == 0 {
+// 				isPrime = false
+// 				break
+// 			}
+// 		}
+// 		if isPrime {
+// 			primeChan <- num
+// 		}
+// 	}
+// 	exitChan <- true
+// }
+
+// func printPrime(primeChan chan int, wg *sync.WaitGroup) {
+// 	defer wg.Done()
+// 	for prime := range primeChan {
+// 		fmt.Println(prime)
+// 	}
+// }
+
+// func main() {
+// 	start := time.Now()
+// 	var wg sync.WaitGroup
+// 	workerCount := 4
+// 	numChan := make(chan int, 1e3)
+// 	primeChan := make(chan int, 2e4)
+// 	exitChan := make(chan bool, 8)
+
+// 	wg.Add(1)
+// 	go generateNumbers(numChan, &wg)
+
+// 	for range workerCount {
+// 		wg.Add(1)
+// 		go filterPrimes(numChan, primeChan, exitChan, &wg)
+// 	}
+
+// 	wg.Add(1)
+// 	go printPrime(primeChan, &wg)
+
+// 	wg.Go(func() {
+// 		for range workerCount {
+// 			<-exitChan
+// 		}
+// 		close(primeChan)
+// 	})
+
+// 	wg.Wait()
+
+//		fmt.Println(time.Since(start))
+//	}
+// package main
+
+// import (
+// 	"fmt"
+// 	"sync"
+// 	"time"
+// )
+
+// func generateNumbers(numChan chan int, wg *sync.WaitGroup) {
+// 	defer wg.Done()
+// 	for num := 2; num < 1.2e5; num++ {
+// 		numChan <- num
+// 	}
+// 	close(numChan)
+// }
+
+// func filterPrimes(numChan chan int, primeChan chan int, exitChan chan bool, wg *sync.WaitGroup) {
+// 	defer wg.Done()
+// 	for num := range numChan {
+// 		isPrime := true
+// 		for i := 2; i*i <= num; i++ {
+// 			if num%i == 0 {
+// 				isPrime = false
+// 				break
+// 			}
+// 		}
+// 		if isPrime {
+// 			primeChan <- num
+// 		}
+// 	}
+// 	exitChan <- true
+// }
+
+// func countPrimes(primeChan chan int, wg *sync.WaitGroup) {
+// 	defer wg.Done()
+// 	count := 0
+// 	for range primeChan {
+// 		count++
+// 	}
+// 	fmt.Printf("在 120000 内共找到 %d 个素数。\n", count)
+// }
+
+// func main() {
+// 	start := time.Now()
+// 	var wg sync.WaitGroup
+// 	workerCount := 10
+// 	numChan := make(chan int, 1000)
+// 	primeChan := make(chan int, 1000)
+// 	exitChan := make(chan bool, workerCount)
+
+// 	wg.Add(1)
+// 	go generateNumbers(numChan, &wg)
+
+// 	for range workerCount {
+// 		wg.Add(1)
+// 		go filterPrimes(numChan, primeChan, exitChan, &wg)
+// 	}
+
+// 	wg.Add(1)
+// 	go countPrimes(primeChan, &wg)
+
+// 	wg.Go(func() {
+// 		for range workerCount {
+// 			<-exitChan
+// 		}
+// 		close(primeChan)
+// 	})
+
+//		wg.Wait()
+//		fmt.Println("耗时:", time.Since(start))
+//	}
 package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func generateNumbers(numChan chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for num := 2; num < 120; num++ {
+		numChan <- num
+	}
+	close(numChan)
+}
+
+func filterPrimes(numChan chan int, primeChan chan int, exitChan chan bool, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for num := range numChan {
+		isPrime := true
+		for i := 2; i*i <= num; i++ {
+			if num%i == 0 {
+				isPrime = false
+				break
+			}
+		}
+		if isPrime {
+			primeChan <- num
+		}
+	}
+	exitChan <- true
+}
+
+func printPrimes(primeChan chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for prime := range primeChan {
+		fmt.Print(prime, " ")
+	}
+}
+
+func main() {
+	start := time.Now()
+	var wg sync.WaitGroup
+	workerCount := 5
+	numChan := make(chan int, 10)
+	primeChan := make(chan int, 10)
+	exitChan := make(chan bool, workerCount)
+
+	wg.Add(1)
+	go generateNumbers(numChan, &wg)
+
+	for range workerCount {
+		wg.Add(1)
+		go filterPrimes(numChan, primeChan, exitChan, &wg)
+	}
+
+	wg.Add(1)
+	go printPrimes(primeChan, &wg)
+
+	wg.Go(func() {
+		for range workerCount {
+			<-exitChan
+		}
+		close(primeChan)
+	})
+
+	wg.Wait()
+
+	fmt.Println()
+	fmt.Println(time.Since(start))
+}
