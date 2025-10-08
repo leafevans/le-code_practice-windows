1bc08256d5c700d93dac005826e01eaf109216cc
LeafEvans <140964616+LeafEvans@users.noreply.github.com>
2025-10-08 19:01:20 +0800

    Update files

--- practice/sql/mysql/test_2025_10_08.sql
+++ practice/sql/mysql/test_2025_10_08.sql
@@ -18,8 +18,29 @@ FROM 'hachimi' @'%';
 
 USE itcast;
 
-SELECT CONCAT("Hello", " World!");
+SELECT CONCAT('Hello', ' World!');
 
-SELECT LOWER("Hello World!");
+SELECT LOWER('Hello World!');
 
-SELECT UPPER("Hello World!");
+SELECT UPPER('Hello World!');
+
+SELECT LPAD('Hello World!', 15, '-*');
+
+SELECT RPAD('Hello World!', 15, '-*');
+
+SELECT TRIM('   Hello World!   ');
+
+SELECT SUBSTRING("Hello World!", 1, 5);
+
+UPDATE emp
+SET workno = LPAD(workno, 5, '0');
+
+SELECT CEILING(3.14);
+
+SELECT FLOOR(3.14);
+
+SELECT MOD(10, 3);
+
+SELECT RAND();
+
+SELECT ROUND(3.1415926, 3);
