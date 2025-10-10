05ad63634da7b8b8dc02b511e61b19772a36e527
LeafEvans <140964616+LeafEvans@users.noreply.github.com>
2025-10-10 13:36:33 +0800

    Update files

--- practice/sql/mysql/test_2025_10_10_11_08.sql
+++ practice/sql/mysql/test_2025_10_10_11_08.sql
@@ -1,11 +1,38 @@
-CREATE TABLE student(
+CREATE TABLE IF NOT EXISTS student (
     id INT AUTO_INCREMENT PRIMARY KEY COMMENT '主键ID',
     name VARCHAR(10) COMMENT '姓名',
     no VARCHAR(10) COMMENT '学号'
 ) COMMENT = '学生表';
 
 INSERT INTO student
-VALUES(NULL, '黛绮丝', '2000100101'),
+VALUES (NULL, '黛绮丝', '2000100101'),
     (NULL, '谢逊', '2000100102'),
     (NULL, '殷天正', '2000100103'),
     (NULL, '韦一笑', '2000100104');
+
+CREATE TABLE IF NOT EXISTS course (
+    id INT AUTO_INCREMENT PRIMARY KEY COMMENT '主键ID',
+    name VARCHAR(10) COMMENT '课程名称'
+) COMMENT = '课程表';
+
+INSERT INTO course
+VALUES (NULL, 'Java'),
+    (NULL, 'PHP'),
+    (NULL, 'MySQL'),
+    (NULL, 'Hadoop');
+
+CREATE TABLE IF NOT EXISTS student_course (
+    id INT AUTO_INCREMENT PRIMARY KEY COMMENT '主键',
+    studentid INT NOT NULL COMMENT '学生ID',
+    courseid INT NOT NULL COMMENT '课程ID',
+    CONSTRAINT fk_courseid FOREIGN KEY (courseid) REFERENCES course (id),
+    CONSTRAINT fk_studentid FOREIGN KEY (studentid) REFERENCES student (id)
+) COMMENT = '学生课程中间表';
+
+INSERT INTO student_course
+VALUES (NULL, 1, 1),
+    (NULL, 1, 2),
+    (NULL, 1, 3),
+    (NULL, 2, 2),
+    (NULL, 2, 3),
+    (NULL, 3, 4);
---
+++ practice/sql/mysql/test_2025_10_10_12_52.sql
@@ -0,0 +1,96 @@
+CREATE TABLE tb_user (
+    id INT AUTO_INCREMENT PRIMARY KEY COMMENT '主键ID',
+    name VARCHAR(10) COMMENT '姓名',
+    age INT COMMENT '年龄',
+    gender CHAR(1) COMMENT '1: 男, 2: 女',
+    phone CHAR(11) COMMENT '手机号'
+) COMMENT = '用户基本信息表';
+
+CREATE TABLE IF NOT EXISTS tb_user_edu (
+    id INT AUTO_INCREMENT PRIMARY KEY COMMENT '主键ID',
+    degree VARCHAR(20) COMMENT '学历',
+    major VARCHAR(50) COMMENT '专业',
+    primaryschool VARCHAR(50) COMMENT '小学',
+    middleschool VARCHAR(50) COMMENT '中学',
+    university VARCHAR(50) COMMENT '大学',
+    userid INT UNIQUE COMMENT '用户ID',
+    CONSTRAINT fk_userid FOREIGN KEY (userid) REFERENCES tb_user (id)
+) COMMENT = '用户教育信息表';
+
+INSERT INTO
+    tb_user (id, name, age, gender, phone)
+VALUES (
+        NULL,
+        '黄渤',
+        45,
+        '1',
+        '18800001111'
+    ),
+    (
+        NULL,
+        '冰冰',
+        35,
+        '2',
+        '18800002222'
+    ),
+    (
+        NULL,
+        '码云',
+        55,
+        '1',
+        '18800008888'
+    ),
+    (
+        NULL,
+        '李彦宏',
+        50,
+        '1',
+        '18800009999'
+    );
+
+INSERT INTO
+    tb_user_edu (
+        id,
+        degree,
+        major,
+        primaryschool,
+        middleschool,
+        university,
+        userid
+    )
+VALUES (
+        NULL,
+        '本科',
+        '舞蹈',
+        '静安区第一小学',
+        '静安区第一中学',
+        '北京舞蹈学院',
+        1
+    ),
+    (
+        NULL,
+        '硕士',
+        '表演',
+        '朝阳区第一小学',
+        '朝阳区第一中学',
+        '北京电影学院',
+        2
+    ),
+    (
+        NULL,
+        '本科',
+        '英语',
+        '杭州市第一小学',
+        '杭州市第一中学',
+        '杭州师范大学',
+        3
+    ),
+    (
+        NULL,
+        '本科',
+        '应用数学',
+        '阳泉第一小学',
+        '阳泉区第一中学',
+        '清华大学',
+        4
+    );
