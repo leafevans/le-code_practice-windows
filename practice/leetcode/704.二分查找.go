e7dd74f3301d853f6b4df8ba282bee37fcc0be3d
LeafEvans <140964616+LeafEvans@users.noreply.github.com>
2025-10-13 00:01:16 +0800

    Update files

--- practice/leetcode/704.二分查找.go
+++ practice/leetcode/704.二分查找.go
@@ -6,7 +6,21 @@
 
 // @lc code=start
 func search(nums []int, target int) int {
+	left := 0
+	right := len(nums)
 
+	for left < right {
+		middle := left + (right-left)>>1
+		if nums[middle] > target {
+			right = middle
+		} else if nums[middle] < target {
+			left = middle + 1
+		} else {
+			return middle
+		}
+	}
+
+	return -1
 }
 
 // @lc code=end
