5b2feb028f23a8c93613241dcbb31ce6337b4b0d
LeafEvans <140964616+LeafEvans@users.noreply.github.com>
2025-10-17 15:00:22 +0800

    Update files

--- practice/go/test_2025_10_17/main.go
+++ practice/go/test_2025_10_17/main.go
@@ -6,12 +6,37 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Article struct {
+	Title   string
+	Content string
+}
+
 func main() {
 	r := gin.Default()
-	r.LoadHTMLGlob("template/*")
+	r.LoadHTMLGlob("templates/**/*")
 	r.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", gin.H{
+		c.HTML(http.StatusOK, "default/index.html", gin.H{
 			"title": "首页",
+		})
+	})
+	r.GET("/news", func(c *gin.Context) {
+		news := &Article{
+			Title:   "新闻标题",
+			Content: "新闻详情",
+		}
+		c.HTML(http.StatusOK, "default/news.html", gin.H{
+			"title": "新闻页面",
+			"news":  news,
+		})
+	})
+	r.GET("/admin", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "admin/news.html", gin.H{
+			"title": "后台首页",
+		})
+	})
+	r.GET("/admin/news", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "admin/news.html", gin.H{
+			"title": "后台新闻页面",
 		})
 	})
 	r.Run()
