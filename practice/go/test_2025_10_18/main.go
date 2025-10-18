69da884eaeff369f57aef25fbc4a542a04fb8aec
LeafEvans <140964616+LeafEvans@users.noreply.github.com>
2025-10-18 20:05:56 +0800

    Update files

--- practice/go/test_2025_10_18/main.go
+++ practice/go/test_2025_10_18/main.go
@@ -1,104 +1,102 @@
-// package main
-
-// import (
-// 	"encoding/xml"
-// 	"net/http"
-
-// 	"github.com/gin-gonic/gin"
-// )
-
-// type UserInfo struct {
-// 	Username string `form:"username" json:"username"`
-// 	Password string `form:"password" json:"password"`
-// }
-
-// type Article struct {
-// 	Title   string `xml:"title"`
-// 	Content string `xml:"content"`
-// }
-
-// func main() {
-// 	r := gin.Default()
-
-// 	r.LoadHTMLGlob("templates/*")
-
-// 	r.GET("/user", func(c *gin.Context) {
-// 		uid := c.Query("uid")
-// 		page := c.DefaultQuery("page", "0")
-// 		c.String(http.StatusOK, "uid = %v, page = %v", uid, page)
-// 	})
-
-// 	r.GET("/user/:uid", func(c *gin.Context) {
-// 		uid := c.Param("uid")
-// 		c.String(http.StatusOK, "userID = %v", uid)
-// 	})
-
-// 	r.GET("/addUser", func(c *gin.Context) {
-// 		c.HTML(http.StatusOK, "default/add_user.html", gin.H{})
-// 	})
-
-// 	r.POST("/doAddUser", func(c *gin.Context) {
-// 		username := c.PostForm("username")
-// 		password := c.PostForm("password")
-// 		age := c.DefaultPostForm("age", "20")
-
-// 		c.JSON(http.StatusOK, gin.H{
-// 			"username": username,
-// 			"password": password,
-// 			"age":      age,
-// 		})
-// 	})
-
-// 	r.GET("/", func(c *gin.Context) {
-// 		userInfo := &UserInfo{}
-// 		if err := c.ShouldBind(userInfo); err != nil {
-// 			c.JSON(http.StatusBadRequest, gin.H{
-// 				"error": err.Error(),
-// 			})
-// 		} else {
-// 			c.JSON(http.StatusOK, userInfo)
-// 		}
-// 	})
-
-// 	r.POST("/doLogin", func(c *gin.Context) {
-// 		userInfo := &UserInfo{}
-// 		if err := c.ShouldBind(userInfo); err != nil {
-// 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-// 		} else {
-// 			c.JSON(http.StatusOK, userInfo)
-// 		}
-// 	})
-
-// 	r.POST("/xml", func(c *gin.Context) {
-// 		data, _ := c.GetRawData()
-// 		a := Article{}
-
-// 		if err := xml.Unmarshal(data, &a); err != nil {
-// 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-// 		} else {
-// 			c.JSON(http.StatusOK, a)
-// 		}
-// 	})
-
-//		r.Run()
-//	}
 package main
 
 import (
-	"test_2025_10_18/routers"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
+type UserInfo struct {
+	Username string `form:"username" json:"username"`
+	Password string `form:"password" json:"password"`
+}
+
+type Article struct {
+	Title   string `xml:"title"`
+	Content string `xml:"content"`
+}
+
 func main() {
 	r := gin.Default()
 
-	routers.DefaultRoutersInit(r)
-	routers.APIRoutersInit(r)
-	routers.AdminRoutersInit(r)
+	r.LoadHTMLGlob("templates/*")
+
+	r.GET("/user", func(c *gin.Context) {
+		uid := c.Query("uid")
+		page := c.DefaultQuery("page", "0")
+		c.String(http.StatusOK, "uid = %v, page = %v", uid, page)
+	})
+
+	r.GET("/user/:uid", func(c *gin.Context) {
+		uid := c.Param("uid")
+		c.String(http.StatusOK, "userID = %v", uid)
+	})
+
+	r.GET("/addUser", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "default/add_user.html", gin.H{})
+	})
+
+	r.POST("/doAddUser", func(c *gin.Context) {
+		username := c.PostForm("username")
+		password := c.PostForm("password")
+		age := c.DefaultPostForm("age", "20")
+
+		c.JSON(http.StatusOK, gin.H{
+			"username": username,
+			"password": password,
+			"age":      age,
+		})
+	})
+
+	r.GET("/", func(c *gin.Context) {
+		userInfo := &UserInfo{}
+		if err := c.ShouldBind(userInfo); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+		} else {
+			c.JSON(http.StatusOK, userInfo)
+		}
+	})
+
+	r.POST("/doLogin", func(c *gin.Context) {
+		userInfo := &UserInfo{}
+		if err := c.ShouldBind(userInfo); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		} else {
+			c.JSON(http.StatusOK, userInfo)
+		}
+	})
+
+	r.POST("/xml", func(c *gin.Context) {
+		a := &Article{}
+		if err := c.ShouldBindXML(a); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		} else {
+			c.JSON(http.StatusOK, a)
+		}
+	})
 
 	r.Run()
 }
+
+// package main
+
+// import (
+// 	"test_2025_10_18/routers"
+
+// 	"github.com/gin-gonic/gin"
+// )
+
+// func main() {
+// 	r := gin.Default()
+
+// 	routers.DefaultRoutersInit(r)
+// 	routers.APIRoutersInit(r)
+// 	routers.AdminRoutersInit(r)
+
+// 	r.Run()
+// }
 
 // package main
 
