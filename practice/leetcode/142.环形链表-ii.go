a1128b239fd6307e84d051ce6c9eb8529abf5440
LeafEvans <140964616+LeafEvans@users.noreply.github.com>
2025-10-30 15:49:07 +0800

    Update files

---
+++ practice/leetcode/142.环形链表-ii.go
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=142 lang=golang
+ *
+ * [142] 环形链表 II
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func detectCycle(head *ListNode) *ListNode {
+	fast := head
+	slow := head
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+		if fast == slow {
+			slow = head
+			for fast != slow {
+				fast = fast.Next
+				slow = slow.Next
+			}
+			return slow
+		}
+	}
+	return nil
+}
+
+// @lc code=end
+
