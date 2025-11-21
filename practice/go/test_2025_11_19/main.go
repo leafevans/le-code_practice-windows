029da33e4cc2a7a8aedfda71952ac17119a30041
LeafEvans <140964616+LeafEvans@users.noreply.github.com>
2025-11-21 18:02:43 +0800

    Update files

--- practice/go/test_2025_11_19/main.go
+++ practice/go/test_2025_11_19/main.go
@@ -1,10 +1,15 @@
+// package main
+
+// import "fmt"
+
+//	func main() {
+//		num := -4.2 + 101i
+//		str := "Hello World!"
+//		fmt.Printf("%.2E\n", num)
+//		fmt.Printf("%04.2s\n", str)
+//	}
 package main
 
-import "fmt"
-
 func main() {
-	num := -4.2 + 101i
-	str := "Hello World!"
-	fmt.Printf("%.2E\n", num)
-	fmt.Printf("%04.2s\n", str)
+
 }
