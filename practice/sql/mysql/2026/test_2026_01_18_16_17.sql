4b9724471a79d964e7c14b32e060102456a1992b
LeafEvans <140964616+LeafEvans@users.noreply.github.com>
2026-01-18 19:52:56 +0800

    Update files

--- practice/sql/mysql/2026/test_2026_01_18_16_17.sql
+++ practice/sql/mysql/2026/test_2026_01_18_16_17.sql
@@ -1,6 +1,7 @@
--- 查询建表语句
--- 默认存储引擎：InnoDB
-SHOW CREATE TABLE account;
-
+-- Active: 1768729232514@@127.0.0.1@3306
 -- 查看当前数据库支持的存储引擎
 SHOW ENGINES;
+
+CREATE TABLE my_myisam (id INT, name VARCHAR(10)) ENGINE = MyISAM;
+
+CREATE TABLE my_memory (id INT, name VARCHAR(10)) ENGINE = Memory;
