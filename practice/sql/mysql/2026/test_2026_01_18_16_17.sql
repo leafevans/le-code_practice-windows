b2e76c8b56ab6960dee5e7733139947614427463
LeafEvans <140964616+LeafEvans@users.noreply.github.com>
2026-01-19 18:12:34 +0800

    Update files

--- practice/sql/mysql/2026/test_2026_01_18_16_17.sql
+++ practice/sql/mysql/2026/test_2026_01_18_16_17.sql
@@ -1,7 +1,9 @@
--- Active: 1768729232514@@127.0.0.1@3306
+-- Active: 1768800322203@@127.0.0.1@3307@practice
 -- 查看当前数据库支持的存储引擎
 SHOW ENGINES;
 
 CREATE TABLE my_myisam (id INT, name VARCHAR(10)) ENGINE = MyISAM;
 
 CREATE TABLE my_memory (id INT, name VARCHAR(10)) ENGINE = Memory;
+
+CREATE TABLE my_innodb (id INT, name VARCHAR(10)) ENGINE = InnoDB;
