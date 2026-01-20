8cc86190650d4b3448959fb364e750297bf47572
LeafEvans <140964616+LeafEvans@users.noreply.github.com>
2026-01-21 02:03:17 +0800

    Update files

--- practice/leetcode/128.最长连续序列.cpp
+++ practice/leetcode/128.最长连续序列.cpp
@@ -5,11 +5,29 @@
  */
 
 // @lc code=start
+#include <algorithm>
+#include <unordered_set>
+#include <vector>
+using namespace std;
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        
+        if (nums.empty()) {
+            return 0;
+        }
+        unordered_set<int> s(nums.begin(), nums.end());
+        int maxLen = 0;
+        for (int num : s) {
+            if (!s.count(num - 1)) {
+                int cur = num, curLen = 1;
+                while (s.count(cur + 1)) {
+                    ++cur;
+                    ++curLen;
+                }
+                maxLen = max(maxLen, curLen);
+            }
+        }
+        return maxLen;
     }
 };
 // @lc code=end
-
