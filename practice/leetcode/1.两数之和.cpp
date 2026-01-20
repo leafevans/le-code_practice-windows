7177e5d971ff1689ccbaadfda95985edde4dcbb1
LeafEvans <140964616+LeafEvans@users.noreply.github.com>
2026-01-20 23:35:34 +0800

    Update files

---
+++ practice/leetcode/1.两数之和.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=cpp
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> map;
+        for (int i = 0; i < nums.size(); ++i) {
+            int complement = target - nums[i];
+            if (map.find(complement) != map.end()) {
+                return {complement, nums[i]};
+            }
+            map[nums[i]] = i;
+        }
+        return {};
+    }
+};
+// @lc code=end
