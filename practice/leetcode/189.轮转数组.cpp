149627bd6a0767b7e83de8619bbf08acbb995a2c
LeafEvans <140964616+LeafEvans@users.noreply.github.com>
2026-01-25 06:53:15 +0800

    Update files

---
+++ practice/leetcode/189.轮转数组.cpp
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=189 lang=cpp
+ *
+ * [189] 轮转数组
+ */
+
+// @lc code=start
+#include <algorithm>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        k %= nums.size();
+        if (k == 0) {
+            return;
+        }
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin() + k);
+        reverse(nums.begin() + k, nums.end());
+    }
+};
+// @lc code=end
---
+++ practice/leetcode/238.除自身以外数组的乘积.cpp
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=238 lang=cpp
+ *
+ * [238] 除自身以外数组的乘积
+ */
+
+// @lc code=start
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> res(n, 1);
+        int left = 1;
+        for (int i = 0; i < n; ++i) {
+            res[i] = left;
+            left *= nums[i];
+        }
+        int right = 1;
+        for (int i = n - 1; i >= 0; --i) {
+            res[i] *= right;
+            right *= nums[i];
+        }
+        return res;
+    }
+};
+// @lc code=end
