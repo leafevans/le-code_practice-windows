a19cbc151439d5015a64924e6983c717275d4b9a
LeafEvans <140964616+LeafEvans@users.noreply.github.com>
2026-01-26 03:18:48 +0800

    Update files

---
+++ practice/leetcode/54.螺旋矩阵.cpp
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode.cn id=54 lang=cpp
+ *
+ * [54] 螺旋矩阵
+ */
+
+// @lc code=start
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        if (matrix.empty() || matrix[0].empty()) {
+            return {};
+        }
+        vector<int> res;
+        int m = matrix.size(), n = matrix[0].size();
+        int top = 0, bottom = m - 1, left = 0, right = n - 1;
+        while (top <= bottom && left <= right) {
+            for (int j = left; j <= right; ++j) {
+                res.push_back(matrix[top][j]);
+            }
+            ++top;
+            for (int i = top; i <= bottom; ++i) {
+                res.push_back(matrix[i][right]);
+            }
+            --right;
+            if (top <= bottom) {
+                for (int j = right; j >= left; --j) {
+                    res.push_back(matrix[bottom][j]);
+                }
+                --bottom;
+            }
+            if (left <= right) {
+                for (int i = bottom; i >= top; --i) {
+                    res.push_back(matrix[i][left]);
+                }
+                ++left;
+            }
+        }
+        return res;
+    }
+};
+// @lc code=end
---
+++ practice/leetcode/73.矩阵置零.cpp
@@ -0,0 +1,64 @@
+/*
+ * @lc app=leetcode.cn id=73 lang=cpp
+ *
+ * [73] 矩阵置零
+ */
+
+// @lc code=start
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        if (matrix.empty() || matrix[0].empty()) {
+            return;
+        }
+        int m = matrix.size(), n = matrix[0].size();
+        bool firstRowZero = false, firstColZero = false;
+        for (int j = 0; j < n; ++j) {
+            if (matrix[0][j] == 0) {
+                firstRowZero = true;
+                break;
+            }
+        }
+        for (int i = 0; i < m; ++i) {
+            if (matrix[i][0] == 0) {
+                firstColZero = true;
+                break;
+            }
+        }
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        for (int i = 1; i < m; ++i) {
+            if (matrix[i][0] == 0) {
+                for (int j = 1; j < n; ++j) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        for (int j = 1; j < n; ++j) {
+            if (matrix[0][j] == 0) {
+                for (int i = 1; i < m; ++i) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if (firstRowZero) {
+            for (int j = 0; j < n; ++j) {
+                matrix[0][j] = 0;
+            }
+        }
+        if (firstColZero) {
+            for (int i = 0; i < m; ++i) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+};
+// @lc code=end
