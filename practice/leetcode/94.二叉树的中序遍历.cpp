ef30fd8ec42924b5c422eed68ccc447f8ffdc071
LeafEvans <140964616+LeafEvans@users.noreply.github.com>
2026-02-09 12:53:14 +0800

    Update files

---
+++ practice/leetcode/94.二叉树的中序遍历.cpp
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=94 lang=cpp
+ *
+ * [94] 二叉树的中序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+#include <stack>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+        stack<TreeNode*> stk;
+        TreeNode* curr = root;
+        while (curr || !stk.empty()) {
+            while (curr) {
+                stk.push(curr);
+                curr = curr->left;
+            }
+            curr = stk.top();
+            stk.pop();
+            res.push_back(curr->val);
+            curr = curr->right;
+        }
+        return res;
+    }
+};
+// @lc code=end
