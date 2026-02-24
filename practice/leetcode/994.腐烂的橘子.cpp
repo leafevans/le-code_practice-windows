26e795d6257f09144b7d8a877ea92a88813831ed
LeafEvans <140964616+LeafEvans@users.noreply.github.com>
2026-02-24 23:30:43 +0800

    Update files

---
+++ practice/leetcode/994.腐烂的橘子.cpp
@@ -0,0 +1,60 @@
+/*
+ * @lc app=leetcode.cn id=994 lang=cpp
+ *
+ * [994] 腐烂的橘子
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        queue<pair<int, int>> q;
+        int fresh = 0;
+
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] == 2) {
+                    q.push({i, j});
+                } else if (grid[i][j] == 1) {
+                    ++fresh;
+                }
+            }
+        }
+
+        if (!fresh) {
+            return 0;
+        }
+
+        int minutes = 0;
+        vector<pair<int, int>> dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+        while (!q.empty()) {
+            int sz = q.size();
+            bool rotted = false;
+
+            for (int i = 0; i < sz; ++i) {
+                auto [x, y] = q.front();
+                q.pop();
+
+                for (auto& dir : dirs) {
+                    int nx = x + dir.first;
+                    int ny = y + dir.second;
+                    if (nx >= 0 && nx < m && ny >= 0 && ny < n && grid[nx][ny] == 1) {
+                        grid[nx][ny] = 2;
+                        q.push({nx, ny});
+                        --fresh;
+                        rotted = true;
+                    }
+                }
+            }
+
+            if (rotted) {
+                ++minutes;
+            }
+        }
+
+        return fresh ? -1 : minutes;
+    }
+};
+// @lc code=end
