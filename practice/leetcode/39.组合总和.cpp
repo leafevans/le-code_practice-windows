f6fdf63c55b5fda599517bbae7c56bba8c810d58
LeafEvans <140964616+LeafEvans@users.noreply.github.com>
2026-02-26 23:10:56 +0800

    Update files

---
+++ practice/leetcode/39.组合总和.cpp
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=39 lang=cpp
+ *
+ * [39] 组合总和
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        backtrack(candidates, target, 0);
+        return res;
+    }
+
+private:
+    vector<vector<int>> res;
+    vector<int> path;
+
+    void backtrack(vector<int>& candidates, int target, int start) {
+        if (target < 0) {
+            return;
+        }
+        if (target == 0) {
+            res.push_back(path);
+        }
+
+        for (int i = start; i < candidates.size(); ++i) {
+            path.push_back(candidates[i]);
+            backtrack(candidates, target - candidates[i], i);
+            path.pop_back();
+        }
+    }
+};
+// @lc code=end
