10f990bd4bc7faf55274231f82fafae9e396c19d
LeafEvans <140964616+LeafEvans@users.noreply.github.com>
2026-03-10 17:01:02 +0800

    Update files

--- practice/leetcode/35.搜索插入位置.cpp
+++ practice/leetcode/35.搜索插入位置.cpp
@@ -8,8 +8,18 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        
+        int l = 0, r = nums.size();
+
+        while (l < r) {
+            int mid = l + ((r - l) >> 1);
+            if (nums[mid] < target) {
+                l = mid + 1;
+            } else {
+                r = mid;
+            }
+        }
+
+        return l;
     }
 };
 // @lc code=end
-
