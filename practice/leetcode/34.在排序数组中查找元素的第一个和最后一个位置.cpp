0bf0a66bd819da2119c7e029f0cbcab15d03efa5
LeafEvans <140964616+LeafEvans@users.noreply.github.com>
2026-03-10 18:10:09 +0800

    Update files

---
+++ practice/leetcode/34.在排序数组中查找元素的第一个和最后一个位置.cpp
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode.cn id=34 lang=cpp
+ *
+ * [34] 在排序数组中查找元素的第一个和最后一个位置
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int start = lowerBound(nums, target);
+
+        if (start == nums.size() || nums[start] != target) {
+            return {-1, -1};
+        }
+
+        int end = lowerBound(nums, target + 1) - 1;
+
+        return {start, end};
+    }
+
+private:
+    int lowerBound(vector<int>& nums, int target) {
+        int l = 0, r = nums.size();
+
+        while (l < r) {
+            int mid = l + ((r - l) >> 1);
+
+            if (nums[mid] < target) {
+                l = mid + 1;
+            } else {
+                r = mid;
+            }
+        }
+
+        return l;
+    }
+};
+// @lc code=end
