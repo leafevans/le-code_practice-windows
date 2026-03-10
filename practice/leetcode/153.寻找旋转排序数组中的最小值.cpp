d85044f04e494076006f0d5c529e5abf634f1c8a
LeafEvans <140964616+LeafEvans@users.noreply.github.com>
2026-03-10 19:44:48 +0800

    Update files

---
+++ practice/leetcode/153.寻找旋转排序数组中的最小值.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=153 lang=cpp
+ *
+ * [153] 寻找旋转排序数组中的最小值
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int l = 0, r = nums.size() - 1;
+
+        while (l < r) {
+            int mid = l + ((r - l) >> 1);
+
+            if (nums[mid] < nums[r]) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+
+        return nums[l];
+    }
+};
+// @lc code=end
