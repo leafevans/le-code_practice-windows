5a1e72d65f190d70da41f9e42143185b83a51535
LeafEvans <140964616+LeafEvans@users.noreply.github.com>
2026-03-23 00:55:00 +0800

    Update files

--- practice/leetcode/84.柱状图中最大的矩形.cpp
+++ practice/leetcode/84.柱状图中最大的矩形.cpp
@@ -8,8 +8,24 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        
+        int n = heights.size();
+        vector<int> h(n + 2, 0);
+        for (int i = 0; i < n; ++i) {
+            h[i + 1] = heights[i];
+        }
+        int ans = 0;
+        stack<int> st;
+        st.push(0);
+        for (int i = 1; i < n + 2; ++i) {
+            while (!st.empty() && h[i] < h[st.top()]) {
+                int height = h[st.top()];
+                st.pop();
+                int width = i - st.top() - 1;
+                ans = max(ans, width * height);
+            }
+            st.push(i);
+        }
+        return ans;
     }
 };
 // @lc code=end
-
