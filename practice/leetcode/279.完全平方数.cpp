97f63141477d427587271fda358de07e7df20534
LeafEvans <140964616+LeafEvans@users.noreply.github.com>
2026-03-29 21:18:52 +0800

    Update files

--- practice/leetcode/279.完全平方数.cpp
+++ practice/leetcode/279.完全平方数.cpp
@@ -8,7 +8,7 @@
 class Solution {
 public:
     int numSquares(int n) {
-        vector<int> dp(n + 1, 1e4);
+        vector<int> dp(n + 1, INT_MAX);
         dp[0] = 0;
         for (int i = 1; i <= n; ++i) {
             for (int j = 1; j * j <= i; ++j) {
