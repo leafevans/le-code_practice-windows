a420563408037c6448d3107fa6b6fc2398f1244b
LeafEvans <140964616+LeafEvans@users.noreply.github.com>
2026-03-31 19:39:17 +0800

    Update files

---
+++ practice/leetcode/287.寻找重复数.cpp
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=287 lang=cpp
+ *
+ * [287] 寻找重复数
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow = nums[0], fast = nums[0];
+
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        int ptr1 = nums[0];
+        int ptr2 = slow;
+        while (ptr1 != ptr2) {
+            ptr1 = nums[ptr1];
+            ptr2 = nums[ptr2];
+        }
+
+        return ptr1;
+    }
+};
+// @lc code=end
