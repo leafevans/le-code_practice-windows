a28c44ca1bdec8fcefded87ddc822c1ff77ae9f5
LeafEvans <140964616+LeafEvans@users.noreply.github.com>
2026-04-02 08:24:36 +0800

    Update files

--- practice/leetcode/11.盛最多水的容器.cpp
+++ practice/leetcode/11.盛最多水的容器.cpp
@@ -8,8 +8,7 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int l = 0;
-        int r = height.size() - 1;
+        int l = 0, r = height.size() - 1;
         int maxArea = 0;
         while (l < r) {
             int h = min(height[l], height[r]);
--- practice/leetcode/15.三数之和.cpp
+++ practice/leetcode/15.三数之和.cpp
@@ -15,8 +15,7 @@ public:
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
-            int l = i + 1;
-            int r = n - 1;
+            int l = i + 1, r = n - 1;
             while (l < r) {
                 int sum = nums[i] + nums[l] + nums[r];
                 if (sum == 0) {
